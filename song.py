23eca18227293dc2ef5ff79ee66356b273c9db37
Marcelosgc1 <marcelobtsgc1@gmail.com>
2024-12-21 10:21:18 -0300

    :tada: initial commit!

---
+++ song.py
@@ -0,0 +1,13 @@
+class Song:
+    name = ''
+    album = ''
+    artist = ''
+    num = 0
+    
+    
+    def __init__(self,name,album,artist) -> None:
+        self.name = name
+        self.album = album
+        self.artist = artist
+        self.num = 1
+
